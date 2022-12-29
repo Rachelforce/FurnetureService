91ea656330c25b563862f57092eb60cd7eed97b8
Rachelforce <art.piskov@gmail.com>
2022-12-29 14:31:27 +0400

    Initial commit

---
+++ src/js/elemetsUI.js
@@ -0,0 +1,232 @@
+import {changeObject} from "./scripts";
+
+const arIcon = new URL("../imgs/ar-icon.png", import.meta.url);
+const shareIcon = new URL("../imgs/share.png", import.meta.url);
+const copyIcon = new URL("../imgs/copy.png", import.meta.url);
+const cssURL = new URL("../style.css", import.meta.url);
+
+const QRCode = require("qrcode");
+const imgURL = new URL("../imgs/Chair.webp", import.meta.url);
+
+const chairURL = new URL("../models/chair.glb", import.meta.url);
+const planterURL = new URL("../models/GeoPlanter.glb", import.meta.url);
+const MixerURL = new URL("../models/Mixer.glb", import.meta.url);
+const toyTrainURL = new URL("../models/ToyTrain.glb", import.meta.url);
+const canoeURL = new URL("../models/Canoe.glb", import.meta.url);
+
+const imgChairURL = new URL("../imgs/Chair.webp", import.meta.url);
+const imgMixerURL = new URL("../imgs/Mixer.webp", import.meta.url);
+const imgGeoPlanterURL = new URL("../imgs/GeoPlanter.webp", import.meta.url);
+const imgToyTrainURL= new URL("../imgs/ToyTrain.webp", import.meta.url);
+const imgCanoeURL= new URL("../imgs/Canoe.webp", import.meta.url);
+
+const modelURLs = [chairURL, planterURL, MixerURL, toyTrainURL, canoeURL]
+const imgModelURLs = [imgChairURL, imgGeoPlanterURL, imgMixerURL, imgToyTrainURL, imgCanoeURL]
+class HTMLElements {
+
+    buttons_div;
+
+     create (canvas, usdzURL) {
+        this.loadCSS();
+         canvas.className = "web-you-canvas";
+        this.createButtonsDiv(canvas)
+        this.createARButton(canvas, usdzURL);
+        this.createShareButton(canvas);
+        this.createShareDiv(canvas);
+        this.createObjCarusel(canvas,modelURLs,imgModelURLs)
+    }
+
+    getMobileOperatingSystem() {
+        var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+        if (/android/i.test(userAgent)) {
+            return "android";
+        }
+
+        if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+            return "ios";
+        }
+        return "pc";
+    }
+
+    createButtonsDiv (canvas){
+        this.buttons_div = document.createElement("div")
+        this.buttons_div.id = "web-you-buttons-div"
+        canvas.appendChild(this.buttons_div);
+    }
+
+    createObjCarusel(canvas, models){
+
+         const imgs = this.httpGet("http://225266.fornex.cloud:8000/files");
+         console.log(imgs);
+         let slider = document.createElement("div");
+        slider.id = "slider";
+        let slides = document.createElement("div");
+        slides.id = "slides";
+        slider.appendChild(slides);
+         canvas.appendChild(slider);
+
+         for (let i =0; i<imgs.length;i++){
+             const fileName = imgs[i].split(".")[0]
+             let objdiv = document.createElement("button");
+             objdiv.style = "background-image: url('http://225266.fornex.cloud:8000/previews/"+ imgs[i] +"');"
+             objdiv.id = i;
+             objdiv.onclick= function (){ changeObject(this, "http://225266.fornex.cloud:8000/models/"+fileName+".glb")};
+             if (i===0){
+                 objdiv.className = "slide selected";
+             }else{
+                 objdiv.className = "slide";
+             }
+             slides.appendChild(objdiv);
+         }
+
+}
+     httpGet(theUrl)
+    {
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+        xmlHttp.send( null );
+        return JSON.parse(xmlHttp.responseText);
+    }
+
+    createARButton(canvas, usdzURL) {
+        const platform = this.getMobileOperatingSystem();
+        if (platform === "ios") {
+            this.createARButtonIOS(canvas, usdzURL);
+        } else if (platform === "android") {
+            this.createARButtonAndroid(canvas);
+        } else if (platform === "pc") {
+            this.createARButtonWindows(canvas);
+        }
+    }
+
+    createARButtonIOS(canvas, usdzURL) {
+        let link = document.createElement("a");
+        link.className = "button";
+        link.id = "link";
+        link.href = usdzURL;
+        link.download = "scene.usdz";
+        link.rel = 'ar';
+        let img = document.createElement('img');
+        img.src = arIcon.href;
+        link.appendChild(img);
+        this.buttons_div.appendChild(link);
+    }
+
+    createARButtonAndroid(canvas) {
+        this.createARButtonWindows(canvas)
+    }
+
+    createARButtonWindows(canvas) {
+        this.createQRCodeDiv(canvas);
+        let link = document.createElement("a");
+        link.id = "arButton";
+        link.className = "web-you-button";
+        link.onclick = this.changeQRDivVisibility;
+        let img = document.createElement('img');
+        img.src = arIcon.href;
+
+        link.appendChild(img);
+        this.buttons_div.appendChild(link);
+    }
+
+
+
+    changeQRDivVisibility() {
+        console.log("click");
+        let divQR = document.getElementById("divQR");
+        if (divQR.style.display === "block") {
+            divQR.style.display = "none";
+        } else {
+            divQR.style.display = "block";
+        }
+    }
+
+    createQRCodeDiv(canvas) {
+        let divQR = document.createElement("div");
+        divQR.id = "divQR";
+
+        let img = document.createElement('img');
+        let divText = document.createElement("div");
+        let textNode = document.createTextNode("Scan this to see model in AR.");
+
+        const QRCode = require('qrcode')
+        var opts = {
+            errorCorrectionLevel: 'H',
+            type: 'image/jpeg',
+            quality: 2,
+            margin: 0,
+            color: {
+                dark: "#000000",
+                light: "#FFFFFF"
+            }
+        }
+
+        QRCode.toDataURL('I am a pony!', opts, function (err, url) {
+            img.src = url;
+        })
+
+        divText.appendChild(textNode);
+        divQR.appendChild(divText);
+        divQR.appendChild(img);
+        canvas.appendChild(divQR);
+    }
+
+    createShareButton(canvas) {
+        let button = document.createElement("a");
+        button.id = "shareButton";
+        button.className = "web-you-button";
+        button.onclick = this.changeShareDivVisibility;
+
+        let img = document.createElement('img');
+        img.src = shareIcon.href;
+
+        button.appendChild(img);
+        this.buttons_div.appendChild(button)
+    }
+
+    createShareDiv(canvas){
+        let div = document.createElement("div");
+        div.id = "shareDiv";
+
+        let textDiv = document.createElement("h1");
+        let codeDive = document.createElement("div");
+        let textNode = document.createTextNode("Paste the following code on any site.");
+        textDiv.appendChild(textNode);
+        let embedCodeText = "<div id=\"web-you-3d-container\" data-uuid=\"759dd2b0-f073-013a-2a70-4e5fca380ce0\"></div><script src=\""+""+"\" type=\"module\"></script>";
+        let embedCode = document.createTextNode(embedCodeText);
+        codeDive.appendChild(embedCode);
+
+        div.appendChild(textDiv);
+        div.appendChild(codeDive);
+        canvas.appendChild(div);
+    }
+
+    changeShareDivVisibility() {
+        console.log("click");
+        let divQR = document.getElementById("shareDiv");
+        if (divQR.style.display === "block") {
+            divQR.style.display = "none";
+        } else {
+            divQR.style.display = "block";
+        }
+
+    }
+
+    loadCSS() {
+        var cssId = 'web-you-3d-container-css';  // you could encode the css path itself to generate id..
+        if (!document.getElementById(cssId)) {
+            var head = document.getElementsByTagName('head')[0];
+            var link = document.createElement('link');
+            link.id = cssId;
+            link.rel = 'stylesheet';
+            link.type = 'text/css';
+            link.href = cssURL;
+            link.media = 'all';
+            head.appendChild(link);
+        }
+    }
+
+}
+
+export {HTMLElements};
---
+++ src/js/scripts.js
@@ -0,0 +1,164 @@
+import * as THREE from 'three';
+import {Vector3} from 'three';
+import {OrbitControls} from "three/examples/jsm/controls/OrbitControls.js";
+import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js';
+import {USDZExporter} from "three/examples/jsm/exporters/USDZExporter";
+import { RoomEnvironment } from 'three/examples/jsm/environments/RoomEnvironment.js';
+
+
+import Stats from "three/examples/jsm/libs/stats.module";
+import {HTMLElements} from "./elemetsUI.js"
+
+const objectURL = new URL("../models/Chair.glb", import.meta.url);
+
+
+let canvas, scene, camera, renderer, controls, light, stats, model, usdzURL, modelID;
+let canvasWidth ;
+let canvasHeight;
+window.addEventListener('resize', onWindowResize, false);
+
+init();
+
+
+function createLight(scene) {
+    light = new THREE.SpotLight(0xc3c3c3, 0.2);
+    light.position.set(2, 2, 2);
+    light.castShadow = true;
+    light.shadow.bias = -0.001;
+    light.shadow.mapSize.width = 1024 * 4;
+    light.shadow.mapSize.height = 1024 * 4;
+    scene.add(light);
+
+    const hemiLight = new THREE.HemisphereLight(0xc3c3c3, 0x040404, 0.2);
+    scene.add(hemiLight);
+}
+
+function createCamera() {
+    camera = new THREE.PerspectiveCamera(60, canvasWidth / canvasHeight, 0.1, 5000);
+    camera.position.set(-2.6, 1, 0)
+
+}
+
+function createFPSCounter(canvas) {
+    stats = new Stats();
+    canvas.appendChild(stats.dom);
+}
+
+function createScene() {
+    scene = new THREE.Scene();
+    const pmremGenerator = new THREE.PMREMGenerator( renderer );
+    scene.environment = pmremGenerator.fromScene( new RoomEnvironment(), 0.04 ).texture;
+    scene.background = new THREE.Color(0xf0f0f0);
+}
+
+function createRenderer(canvas) {
+    renderer = new THREE.WebGLRenderer({antialias: true});
+    renderer.setSize(canvasWidth, canvasHeight);
+    renderer.shadowMap.renderReverseSided = true;
+    renderer.shadowMap.renderSingleSided = false;
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.shadowMap.enabled = true;
+    renderer.gammaOutput = true;
+    renderer.gammaFactor = 2;
+    renderer.outputEncoding = THREE.sRGBEncoding;
+
+    canvas.appendChild(renderer.domElement);
+}
+
+function createOrbitControl(camera, rendererDom) {
+    controls = new OrbitControls(camera, rendererDom);
+    recenterCamera()
+}
+
+async function getGLBModel(objURL) {
+    const loader = new GLTFLoader();
+    let dataGLB = await loader.loadAsync(objURL);
+    model = dataGLB.scene;
+    model.name = "object"
+    model.traverse(n => {
+        if (n.isMesh) {
+            n.castShadow = true;
+            n.receiveShadow = true;
+            if (n.material.map) n.material.map.anisotropy = 1;
+        }
+    })
+    return model;
+}
+
+async function createUSDZFromGLB() {
+    const exporter = new USDZExporter();
+    const arraybuffer = await exporter.parse(model);
+    const usdz = new Blob([arraybuffer], {type: 'application/octet-stream'});
+    return URL.createObjectURL(usdz);
+}
+
+function animate() {
+    renderer.render(scene, camera);
+    light.position.set(
+        camera.position.x - 1,
+        camera.position.y + 1,
+        camera.position.z - 1,
+    );
+    //stats.update();
+    requestAnimationFrame(animate);
+}
+
+function onWindowResize() {
+    camera.aspect = canvas.clientWidth / canvas.clientHeight;
+    camera.updateProjectionMatrix();
+    console.log(canvas.clientWidth + " "+ canvas.clientHeight);
+    renderer.setSize(canvas.clientWidth, canvas.clientHeight);
+}
+
+export async function changeObject(button, modelURL){
+    const slides = document.querySelectorAll(".slide");
+    slides.forEach((element) => {
+        element.classList.remove("selected");
+    });
+    button.className = "slide selected"
+    removeEntity();
+    await addEntity(modelURL);
+    recenterCamera();
+}
+
+function removeEntity() {
+    var selectedObject = scene.getObjectByName("object");
+    scene.remove( selectedObject );
+    animate();
+}
+
+async function addEntity(objURL){
+    model = await getGLBModel(objURL);
+    scene.add(model);
+}
+
+function recenterCamera(){
+    let boundingBox = new THREE.Box3().setFromObject(model);
+    camera.lookAt(model.position.x, model.position.y + (boundingBox.max.y-boundingBox.min.y)/ 2, model.position.z);
+    controls.target.copy(model.position).add(new Vector3(0, (boundingBox.max.y-boundingBox.min.y)/ 2, 0));
+}
+
+async function init() {
+    canvas = document.getElementById("web-you-3d-container");
+
+     canvasWidth = canvas.clientWidth;
+     canvasHeight = canvas.clientHeight;
+    modelID = canvas.dataset.uuid;
+    console.log(canvas.clientWidth);
+    createRenderer(canvas);
+    createScene();
+    createCamera();
+    //createFPSCounter(canvas);
+    createLight(scene);
+
+    await addEntity(objectURL.href);
+
+    usdzURL = await createUSDZFromGLB(model);
+
+    createOrbitControl(camera, renderer.domElement);
+
+    const htmlElements = new HTMLElements();
+    htmlElements.create(canvas, usdzURL);
+    animate();
+
+}
